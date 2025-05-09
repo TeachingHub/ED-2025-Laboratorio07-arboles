e0ca8f7e5aac4f8ffd665cfb39ac7a822f11004b
Sergio Cavero <sergio.cavero@urjc.es>
2025-05-09 23:56:23 +0200

    Added solutions of exercise 1

--- uBinaryTree.pas
+++ uBinaryTree.pas
@@ -23,6 +23,10 @@ interface
   procedure inorder(a: tbinaryTree);
   procedure postorder(a: tbinaryTree);
 
+  // Other methods
+  function get_hi(a:tbinaryTree): tbinaryTree;
+  function get_hd(a:tbinaryTree): tbinaryTree;
+
   // Ejercicios 
 
   {Ejercicio 1.1}
@@ -128,68 +132,111 @@ begin
   end
 end;
 
+
+function get_hi(a:tbinaryTree): tbinaryTree;
+begin
+  if a = nil then get_hi := nil
+  else get_hi:= a^.hi;
+end;
+
+function get_hd(a:tbinaryTree): tbinaryTree;
+begin
+  if a = nil then get_hd := nil
+  else get_hd:= a^.hd;
+end;
+
 { ---------------Ejercicio 1.1------------------ }
 procedure inorder_inverse(a: tbinaryTree);
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a <> NIL) then begin
+    inorder_inverse(a^.hd);
+    visit(a^.info);
+    inorder_inverse(a^.hi)
+  end
 end;
 
 { ---------------Ejercicio 1.2------------------ }
 function profundidad_max(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then profundidad_max := 0
+  else begin
+    profundidad_max := 1 + max(profundidad_max(a^.hi), profundidad_max(a^.hd))
+  end;
 end;
 
 { ---------------Ejercicio 1.3------------------ }
 function node_count(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+    if (a = NIL) then node_count := 0
+    else node_count := 1 + node_count(a^.hd) + node_count(a^.hi)
 end;
 
 { ---------------Ejercicio 1.4------------------ }
 function leafs_count(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then leafs_count := 0
+  else if (a^.hi = NIL) and (a^.hd = NIL) then leafs_count := 1
+  else leafs_count := leafs_count(a^.hi) + leafs_count(a^.hd)
 end;
 
 { ---------------Ejercicio 1.5------------------ }
 function internal_nodes_count(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then internal_nodes_count := 0
+  else if (a^.hi = NIL) and (a^.hd = NIL) then internal_nodes_count := 0
+  else internal_nodes_count := 1 + internal_nodes_count(a^.hi) + internal_nodes_count(a^.hd)
 end;
 
 { ---------------Ejercicio 1.6------------------ }
 function is_full(a: tbinaryTree): boolean;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then
+    is_full := true
+  else if ((a^.hi = NIL) and (a^.hd <> NIL)) or ((a^.hi <> NIL) and (a^.hd = NIL)) then
+    is_full := false
+  else
+    is_full := is_full(a^.hi) and is_full(a^.hd);
 end;
 
   
   { ---------------Ejercicio 1.7------------------ }
 function max_hoja(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then max_hoja := 0
+  else if (a^.hi = NIL) and (a^.hd = NIL) then max_hoja := a^.info
+  else begin
+    max_hoja := max(max_hoja(a^.hi), max_hoja(a^.hd));
+  end;
 end;
 
   
   { ---------------Ejercicio 1.8------------------ }
 function sum_hoja(a:tbinaryTree): integer; 
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then sum_hoja := 0
+  else if (a^.hi = NIL) and (a^.hd = NIL) then sum_hoja := a^.info
+  else begin
+    sum_hoja := sum_hoja(a^.hi) + sum_hoja(a^.hd);
+  end;
+
 end;
 
 
   { ---------------Ejercicio 1.9------------------ }
 function num_pares(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then num_pares := 0
+  else if (a^.info mod 2 = 0) then num_pares := 1 + num_pares(a^.hi) + num_pares(a^.hd)
+  else num_pares := num_pares(a^.hi) + num_pares(a^.hd);
 end;
 
   
     { ---------------Ejercicio 1.10------------------ }
 function num_nodos_en_nivel(a:tbinaryTree; nivel:integer): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a = NIL) then num_nodos_en_nivel := 0
+  else if (nivel = 0) then num_nodos_en_nivel := 1
+  else num_nodos_en_nivel := num_nodos_en_nivel(a^.hi, nivel - 1) + num_nodos_en_nivel(a^.hd, nivel - 1);
 end;
 
 
